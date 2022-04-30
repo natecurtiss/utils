8f256b1638cd6cc5f81d73aa79d4591ca7c2b67a
natecurtiss <n8dev.cs@gmail.com>
2022-04-30 14:41:36 -0400

    Add default folders creation tool

---
+++ Editor/ToolsMenu.cs
@@ -0,0 +1,60 @@
+using UnityEditor;
+using UnityEngine;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static UnityEditor.AssetDatabase;
+
+namespace NateCurtiss.Utils
+{
+    static class ToolsMenu
+    {
+        static readonly string _root = Application.dataPath;
+        
+        [MenuItem("Tools/Setup/Create Default Folders")]
+        static void DefaultFolders()
+        {
+            New("Materials");
+            New("Models");
+            New("Music");
+            New("Plugins");
+            New("Prefabs");
+            New("Scenes");
+            New("Scripts");
+            New("Settings");
+            New("Shaders");
+            New("Sounds");
+            New("Textures");
+            Refresh();
+        }
+        
+        [MenuItem("Tools/Setup/Create Default Folders Grouped")]
+        static void DefaultFoldersGrouped()
+        {
+            New("Art", out var art);
+            New(art, "Materials");
+            New(art, "Models");
+            New(art, "Shaders");
+            New(art, "Textures");
+            
+            New("Audio", out var audio);
+            New(audio, "Sounds");
+            New(audio, "Music");
+
+            New("Code", out var code);
+            New(code, "Editor");
+            New(code, "Runtime");
+
+            New("Plugins");
+            New("Prefabs");
+            New("Scenes");
+            New("Settings");
+            
+            Refresh();
+        }
+
+        static void New(string folder) => New(_root, folder, out _);
+        static void New(string folder, out string path) => New(_root, folder, out path);
+        static void New(string root, string folder) => New(root, folder, out _);
+        static void New(string root, string folder, out string path) => path = CreateDirectory(Combine(root, folder)).FullName;
+    }
+}
